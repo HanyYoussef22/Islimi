0a4eaba57cc29cbc2263f45229b8dff483e6b76e
dell <hanyy81667@gmail.com>
2022-07-19 22:55:59 +0200

    init get empty Screen

---
+++ lib/Home_Layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+static const String roudeName='HomeLayout';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Column(
+
+      ),
+    );
+  }
+}
